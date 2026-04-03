bbc4b722898465e12f97f724d5a8a0fd76e82518
gadirajuieee <gadiraju@ieee.org>
2026-04-02 20:06:51 -0700

    lab2:variables and types practice of different

---
+++ labs/basics/variables.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Prasad Gadiraju"
+	var age int = 41
+	var salary float64 = 1200.00
+	var isActive bool = true
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Salary:", salary)
+	fmt.Println("Active", isActive)
+
+	city := "San Jose"
+	experience := 10
+	rating := 4.9
+
+	fmt.Println("\nCity:", city)
+	fmt.Println("Experience", experience, "years")
+	fmt.Println("Rating:", rating)
+
+}
